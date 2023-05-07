0c12ba9c1b55779eadb8fa0e39d0c9ed563b6465
조해창 <outofchng@gmail.com>
2023-05-08 02:31:00 +0900

    config: 프로젝트 초기 설정

---
+++ src/styles/chakraTheme/index.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const chakraTheme = extendTheme({
+  colors: {
+    customColor: {
+      300: '#A181DF',
+      400: '#A165DF',
+      500: '#A53EDF',
+    },
+    gray: {
+      100: '#CBD5E0',
+    },
+  },
+})

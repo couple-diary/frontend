832c79e59f43d85b4cd50fb93b0c13a15a606ddc
조해창 <outofchng@gmail.com>
2023-05-08 16:07:24 +0900

    feat: 로그인 페이지 ui 퍼블리싱

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,3 +1,30 @@
-import { LoginPage } from '~/components/login'
+import { AbsoluteCenter, Box, Button, Input, InputGroup } from '@chakra-ui/react'
+import { Stack } from '@toss/emotion-utils'
+import { useRouter } from 'next/router'
 
-export default LoginPage
+const Page = () => {
+  const router = useRouter()
+
+  return (
+    <Box position="relative" h="80vh">
+      <AbsoluteCenter axis="both">
+        <Stack as="form" spacing={4}>
+          <InputGroup width="sm">
+            <Input placeholder="닉네임을 입력해주세요" />
+          </InputGroup>
+          <InputGroup>
+            <Input type="password" placeholder="비밀번호를 입력해주세요" />
+          </InputGroup>
+          <Button type="submit" colorScheme="blue">
+            로그인
+          </Button>
+          <Button colorScheme="blue" variant="outline" onClick={() => router.push('/signup')}>
+            회원가입
+          </Button>
+        </Stack>
+      </AbsoluteCenter>
+    </Box>
+  )
+}
+
+export default Page
---
+++ src/pages/signup/index.page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>sign up page </div>
+}
+
+export default Page
